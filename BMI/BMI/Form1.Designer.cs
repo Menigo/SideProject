ee87bfa8cf09e35d9d610b3ee96148c96920e0b3
Menigo <a0986922631@gmail.com>
2023-07-23 04:16:08 +0800

    BmiComplete
    BmiComplete

--- BMI/BMI/Form1.Designer.cs
+++ BMI/BMI/Form1.Designer.cs
@@ -33,75 +33,85 @@
             this.width_txtBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.result_txtBox = new System.Windows.Forms.TextBox();
+            this.result_txtBox = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // result_btn
             // 
-            this.result_btn.Location = new System.Drawing.Point(32, 210);
+            this.result_btn.Location = new System.Drawing.Point(20, 137);
+            this.result_btn.Margin = new System.Windows.Forms.Padding(2);
             this.result_btn.Name = "result_btn";
-            this.result_btn.Size = new System.Drawing.Size(200, 54);
+            this.result_btn.Size = new System.Drawing.Size(127, 35);
             this.result_btn.TabIndex = 0;
             this.result_btn.Text = "計算";
             this.result_btn.UseVisualStyleBackColor = true;
+            this.result_btn.Click += new System.EventHandler(this.result_btn_Click);
             // 
             // height_txtBox
             // 
             this.height_txtBox.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.height_txtBox.Location = new System.Drawing.Point(32, 62);
+            this.height_txtBox.ImeMode = System.Windows.Forms.ImeMode.Alpha;
+            this.height_txtBox.Location = new System.Drawing.Point(20, 40);
+            this.height_txtBox.Margin = new System.Windows.Forms.Padding(2);
             this.height_txtBox.Name = "height_txtBox";
-            this.height_txtBox.Size = new System.Drawing.Size(200, 38);
+            this.height_txtBox.Size = new System.Drawing.Size(129, 28);
             this.height_txtBox.TabIndex = 1;
+            this.height_txtBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.text_KeyPress);
             // 
             // width_txtBox
             // 
             this.width_txtBox.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.width_txtBox.Location = new System.Drawing.Point(32, 145);
+            this.width_txtBox.ImeMode = System.Windows.Forms.ImeMode.Alpha;
+            this.width_txtBox.Location = new System.Drawing.Point(20, 95);
+            this.width_txtBox.Margin = new System.Windows.Forms.Padding(2);
             this.width_txtBox.Name = "width_txtBox";
-            this.width_txtBox.Size = new System.Drawing.Size(200, 38);
+            this.width_txtBox.Size = new System.Drawing.Size(129, 28);
             this.width_txtBox.TabIndex = 2;
+            this.width_txtBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.text_KeyPress);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 36);
+            this.label1.Location = new System.Drawing.Point(20, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 23);
+            this.label1.Size = new System.Drawing.Size(59, 15);
             this.label1.TabIndex = 3;
             this.label1.Text = "身高(CM)";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 119);
+            this.label2.Location = new System.Drawing.Point(20, 78);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 23);
+            this.label2.Size = new System.Drawing.Size(55, 15);
             this.label2.TabIndex = 4;
             this.label2.Text = "體重(KG)";
             // 
             // result_txtBox
             // 
-            this.result_txtBox.Enabled = false;
-            this.result_txtBox.Font = new System.Drawing.Font("Microsoft JhengHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.result_txtBox.Location = new System.Drawing.Point(287, 54);
+            this.result_txtBox.AutoSize = true;
+            this.result_txtBox.Location = new System.Drawing.Point(170, 40);
             this.result_txtBox.Name = "result_txtBox";
-            this.result_txtBox.Size = new System.Drawing.Size(150, 38);
-            this.result_txtBox.TabIndex = 5;
-            this.result_txtBox.Text = "12344";
+            this.result_txtBox.Size = new System.Drawing.Size(55, 15);
+            this.result_txtBox.TabIndex = 6;
+            this.result_txtBox.Text = "尚未計算";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(491, 305);
+            this.ClientSize = new System.Drawing.Size(312, 199);
             this.Controls.Add(this.result_txtBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.width_txtBox);
             this.Controls.Add(this.height_txtBox);
             this.Controls.Add(this.result_btn);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "BMI計算機";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -114,6 +124,6 @@
         private TextBox width_txtBox;
         private Label label1;
         private Label label2;
-        private TextBox result_txtBox;
+        private Label result_txtBox;
     }
 }
--- BMI/BMI/Form1.cs
+++ BMI/BMI/Form1.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace BMI
 {
     public partial class Form1 : Form
@@ -5,6 +7,44 @@ namespace BMI
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void result_btn_Click(object sender, EventArgs e)
+        {
+            float height = float.Parse(height_txtBox.Text) / 100;
+            float weith = float.Parse(width_txtBox.Text);
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            float bmi = weith / (height * height);
+
+            stringBuilder.AppendLine("您的bmi為:" + bmi.ToString("#.#"));
+
+            if(bmi < 18.5)
+                stringBuilder.AppendLine("您的體重過輕");
+            if(bmi >= 18.5 && bmi < 24)
+                stringBuilder.AppendLine("您的體重正常");
+            if(bmi >= 24)
+            {
+                stringBuilder.AppendLine("您的體重過重");
+                if (27 <= bmi && bmi < 30)
+                    stringBuilder.AppendLine("且輕度肥胖");
+                if (30 <= bmi && bmi < 35)
+                    stringBuilder.AppendLine("且中度肥胖");
+                if (35 <= bmi)
+                    stringBuilder.AppendLine("且重度肥胖");
+
+            }
+            result_txtBox.Text = stringBuilder.ToString();
+
+        }
+
+        private void text_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((int)e.KeyChar < 48 | (int)e.KeyChar > 57) & (int)e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
         }
     }
 }
